b217bd908056fd11c411c54f63d785e650c5f5eb
Gabriel Campos <gabrielcmdeveloper@gmail.com>
2025-05-28 18:00:33 +0200

    Updates

--- jest.setup.ts
+++
@@ -1,8 +0,0 @@
-// jest.setup.ts
-import { TextEncoder, TextDecoder as NodeTextDecoder } from "util";
-
-global.TextEncoder = TextEncoder;
-
-if (typeof global.TextDecoder === "undefined") {
-  global.TextDecoder = NodeTextDecoder as unknown as typeof TextDecoder;
-}
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/IKEA-SKAPA-CHALLENGE-FRONTEND-BY-GABRIEL-CAMPOS/',
-  plugins: [react()],
-})
